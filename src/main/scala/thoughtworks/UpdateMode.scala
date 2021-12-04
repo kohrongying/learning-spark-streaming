82376cdf0a4798c2825c52d1481a885b6d626ba8
rongying <rongying.koh@thoughtworks.com>
2021-12-04 18:05:05 +0800

    Try outputMode=update.

---
+++ src/main/scala/thoughtworks/UpdateMode.scala
@@ -0,0 +1,37 @@
+package thoughtworks
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+
+object UpdateMode {
+
+  def main(args: Array[String]): Unit = {
+    // Create Spark Session
+    val spark = SparkSession.builder()
+      .master("local")
+      .appName("Socket Source")
+      .getOrCreate()
+
+    // Set Spark logging level to ERROR.
+    spark.sparkContext.setLogLevel("ERROR")
+
+    val sourceDF = spark.readStream
+      .format("socket")
+      .option("host", "localhost")
+      .option("port", "9999")
+      .load()
+
+    // Split each line into words
+    val words = sourceDF
+      .select(explode(split(col("value"), " ")).as("words"))
+      .groupBy("words")
+      .count()
+
+    // Sink
+    val sink = words.writeStream
+      .outputMode("update")
+      .format("console")
+
+    sink.start().awaitTermination()
+  }
+}
