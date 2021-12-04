c28367a75e5b77272c225e3e7aa4f74850949881
rongying <rongying.koh@thoughtworks.com>
2021-12-04 19:18:41 +0800

    Read stream from input file source (csv)

--- README.md
+++ README.md
@@ -40,7 +40,9 @@ nc -lk 9999
 
 ### 2. File source
 File Data Source Streaming Example using the concept of checkpointing
-
+```aidl
+Base Path error
+```
 #### Checkpointing
 
 ## Kafka
@@ -48,3 +50,4 @@ Kafka Data Source Streaming example
 
 # Resources
 1. [Output Modes](https://spark.apache.org/docs/latest/structured-streaming-programming-guide.html#output-modes)
+2. [Pokemon csv](https://gist.githubusercontent.com/armgilles/194bcff35001e7eb53a2a8b441e8b2c6/raw/92200bc0a673d5ce2110aaad4544ed6c4010f687/pokemon.csv)
---
+++ src/main/scala/thoughtworks/_5InputFileSource.scala
@@ -0,0 +1,54 @@
+package thoughtworks
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+
+object _5InputFileSource {
+
+  def main(args: Array[String]): Unit = {
+    // Create Spark Session
+    val spark = SparkSession.builder()
+      .master("local")
+      .appName("Socket Source")
+      .getOrCreate()
+
+    // Set Spark logging level to ERROR.
+    spark.sparkContext.setLogLevel("ERROR")
+
+    val schema = StructType(List(
+      StructField("#", StringType, true),
+      StructField("Name", StringType, true),
+      StructField("Type 1", StringType, true),
+      StructField("Type 2", StringType, true),
+      StructField("Total", DoubleType, true),
+      StructField("HP", DoubleType, true),
+      StructField("Attack", DoubleType, true),
+      StructField("Defense", DoubleType, true),
+      StructField("Sp. Atk", DoubleType, true),
+      StructField("Sp. Def", DoubleType, true),
+      StructField("Speed", DoubleType, true),
+      StructField("Generation", DoubleType, true),
+      StructField("Legendary", BooleanType, true)
+    ))
+
+    val sourceDF = spark.readStream
+      .format("csv")
+      .option("maxFilesPerTrigger", 2) // This will read maximum of 2 files per mini batch. However, it can read less than 2 files.
+      .option("header", true)
+      .schema(schema)
+      .load("data/pokemon*.csv")
+
+    // Do some transform
+    val pokemon = sourceDF
+      .groupBy("Type 1")
+      .agg(max("HP").as("Tank"))
+
+    // Sink
+    val sink = pokemon.writeStream
+      .outputMode("update")
+      .format("console")
+
+    sink.start().awaitTermination()
+  }
+}
